44370e6d87f4c43f816fc492bda55d7f66822360
doodoodoong <doo_doong@hanmail.net>
2022-12-06 14:43:44 +0900

    상호작용

--- stateful/lib/main.dart
+++ stateful/lib/main.dart
@@ -40,7 +40,7 @@ class _MyHomePageState extends State<MyHomePage> {
         children: <Widget>[
           Column(
             crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
+            children: [
               Text('Oeschinen Lake Campground',
                   style: TextStyle(fontWeight: FontWeight.bold)),
               Text('Kandersteg, Switzerland',
@@ -48,38 +48,216 @@ class _MyHomePageState extends State<MyHomePage> {
             ],
           ),
           Row(
-            children: <Widget>[Icon(Icons.star, color: Colors.red), Text('41')],
+            children: [FavoriteWidget()],
           )
         ],
       ),
     );
-    var buttonSection = Row(
-      children: <Widget>[
-        Column(
-          children: <Widget>[Icon(Icons.call), Text('CALL')],
-        ),
-        Column(
-          children: <Widget>[Icon(Icons.navigation), Text('ROUTE')],
-        ),
-        Column(
-          children: <Widget>[Icon(Icons.share), Text('SHARE')],
-        )
-      ],
-    );
-    var textSection = Container(
+
+    Color color = Theme.of(context).primaryColor;
+
+    var buttonSection = Container(
+      margin: EdgeInsets.all(10),
       child: Row(
-        children: <Widget>[Text('카리나 존나 이쁘네 ㅋㅋㅋㅋㅋ')],
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          Column(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Icon(
+                color: color,
+                Icons.call,
+                size: 40,
+              ),
+              Text(style: TextStyle(color: color), 'CALL')
+            ],
+          ),
+          Column(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Icon(
+                color: color,
+                Icons.navigation,
+                size: 40,
+              ),
+              Text(style: TextStyle(color: color), 'ROUTE')
+            ],
+          ),
+          Column(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Icon(
+                color: color,
+                Icons.share,
+                size: 40,
+              ),
+              Text(style: TextStyle(color: color), 'SHARE')
+            ],
+          )
+        ],
       ),
     );
+    var textSection = Container(
+      padding: EdgeInsets.all(10),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+          Text(
+            '카리나 존나 이쁘네 ㅋㅋㅋㅋㅋ',
+            style: TextStyle(fontWeight: FontWeight.bold),
+          )
+        ],
+      ),
+    );
+
     return Scaffold(
+        appBar: AppBar(title: Text('Karina')),
         body: Column(
-      children: <Widget>[
-        Image.network(
-            'https://www.allkpop.com/upload/2022/02/content/181550/web_data/allkpop_1645217742_untitled-1.jpg'),
-        titleSection,
-        buttonSection,
-        textSection
-      ],
-    ));
+          children: <Widget>[
+            Image.network(
+                'https://www.allkpop.com/upload/2022/02/content/181550/web_data/allkpop_1645217742_untitled-1.jpg'),
+            titleSection,
+            buttonSection,
+            textSection,
+            Row(
+              children: [TapboxA()],
+            )
+          ],
+        ));
+  }
+}
+
+class ParentWidget extends StatefulWidget {
+  const ParentWidget({super.key});
+
+  @override
+  State<ParentWidget> createState() => _ParentWidgetState();
+}
+
+class _ParentWidgetState extends State<ParentWidget> {
+  bool _active = false;
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      child: TapboxB(
+        active: _active,
+        onChanged: _handleTapboxChanged,
+      ),
+    );
+  }
+
+  void _handleTapboxChanged(bool newValue) {
+    setState(() {
+      _active = newValue;
+    });
+  }
+}
+
+class TapboxB extends StatelessWidget {
+  const TapboxB({super.key, this.active = false, required this.onChanged});
+
+  final bool active;
+  final ValueChanged<bool> onChanged;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: _handleTap,
+      child: Container(
+        width: 150,
+        height: 150,
+        decoration: BoxDecoration(
+            color: active ? Colors.lightGreen[700] : Colors.grey[600]),
+        child: Center(
+          child: Text(
+            active ? 'Active' : 'Inactive',
+            style: TextStyle(fontSize: 32, color: Colors.white),
+          ),
+        ),
+      ),
+    );
+  }
+
+  void _handleTap() {
+    onChanged(!active);
+  }
+}
+
+class TapboxA extends StatefulWidget {
+  const TapboxA({super.key});
+
+  @override
+  State<TapboxA> createState() => _TapboxAState();
+}
+
+class _TapboxAState extends State<TapboxA> {
+  bool _active = false;
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: _handleTap,
+      child: Container(
+        width: 150,
+        height: 150,
+        decoration: BoxDecoration(
+            color: _active ? Colors.lightGreen[700] : Colors.grey[600],
+            borderRadius: BorderRadius.all(Radius.circular(10))),
+        child: Center(
+          child: Text(
+            _active ? 'Active' : 'Inactive',
+            style: TextStyle(fontSize: 32, color: Colors.white),
+          ),
+        ),
+      ),
+    );
+  }
+
+  void _handleTap() {
+    setState(() {
+      _active = !_active;
+    });
+  }
+}
+
+class FavoriteWidget extends StatefulWidget {
+  const FavoriteWidget({super.key});
+
+  @override
+  State<FavoriteWidget> createState() => _FavoriteWidgetState();
+}
+
+class _FavoriteWidgetState extends State<FavoriteWidget> {
+  bool _isFavorited = true;
+  int _favoriteCount = 41;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.fromLTRB(0, 0, 20, 0),
+      child: Row(
+        children: [
+          IconButton(
+            icon: (_isFavorited ? Icon(Icons.star) : Icon(Icons.star_border)),
+            color: Colors.red,
+            onPressed: _toggleFavorite,
+          ),
+          Text('$_favoriteCount')
+        ],
+      ),
+    );
+  }
+
+  void _toggleFavorite() {
+    setState(() {
+      if (_isFavorited) {
+        _favoriteCount -= 1;
+        _isFavorited = false;
+      } else {
+        _favoriteCount += 1;
+        _isFavorited = true;
+      }
+    });
   }
 }

1e5c271a0bad72de136af082bd592a0731144e93
doodoodoong <doo_doong@hanmail.net>
2024-01-26 23:14:06 +0900

    stateful

--- first_project/lib/main.dart
+++ first_project/lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MainScreen(),
+      home: const MainScreen2(),
     );
   }
 }
@@ -29,6 +29,39 @@ class MainScreen extends StatelessWidget {
       body: const Text('Hello'),
       appBar: AppBar(
         title: const Text("My first app"),
+      ),
+    );
+  }
+}
+
+class MainScreen2 extends StatefulWidget {
+  const MainScreen2({super.key});
+
+  @override
+  State<MainScreen2> createState() => _MainScreen2State();
+}
+
+class _MainScreen2State extends State<MainScreen2> {
+  String msg = 'Kim';
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(
+      const Duration(seconds: 3),
+      () {
+        setState(() {
+          msg = 'SeongSik';
+        });
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text(msg),
+      appBar: AppBar(
+        title: const Text('My first app'),
       ),
     );
   }
